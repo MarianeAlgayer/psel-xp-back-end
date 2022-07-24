7e5dc268252c28f9ae0b271c7d04c0f8f1706759
Mariane Algayer <marianealgayer@gmail.com>
2022-07-24 00:25:33 -0300

    refactor(services): shares
    Refactor function to return a number on share value key

--- back-end/src/services/shares.service.ts
+++ back-end/src/services/shares.service.ts
@@ -2,7 +2,14 @@ import sharesModel from '../models/shares.model';
 
 import { IShare } from '../@types/interfaces.d';
 
-const getAllShares = (): Promise<IShare[]> => sharesModel.getAllShares();
+const getAllShares = async (): Promise<IShare[]> => {
+  const shares = await sharesModel.getAllShares();
+
+  return shares.map((share) => ({
+    ...share,
+    value: Number(share.value),
+  }));
+};
 
 export default {
   getAllShares,
