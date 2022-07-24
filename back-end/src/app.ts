dc7b05ff9fe0cf24b44dab79a1ba2929550a91d2
Mariane Algayer <marianealgayer@gmail.com>
2022-07-24 00:27:25 -0300

    docs: swagger documentation

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerConfig from './docs/swagger.json';
 
 import routes from './routes';
 import errorMiddleware from './middlewares/error.middleware';
 
 const app = express();
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
 app.use(cors());
 
